6d382b0a5c27d44725fd8744ce8a88a8b83739d5
MarcioNatal <mn_natal@yahoo.com.br>
2025-01-31 17:32:08 -0300

    Create inicializaInterfaceFrc.bat

---
+++ inicializaInterfaceFrc.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\Work\Documents\GitHub\OfficialCode-2025\BaseSwerve2025PoseEstimatorStableV1\src\main\deploy\UIv1"
+start http://localhost:8888
+pynetworktables2js --robot roborio-7563-frc.local
+
+pause
