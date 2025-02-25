a269d7fc8299503e4a3a327320a84b6acc612ddd
Rcastilho03 <rickricardo0303@gmail.com>
2025-02-25 08:43:58 -0300

    New Interface

--- Example/src/main/deploy/UIv1/inicializaInterfaceFrc.bat
+++ Example/src/main/deploy/UIv1/inicializaInterfaceFrc.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Users\Work\Documents\GitHub\OfficialCode-2025\BaseSwerve2025PoseEstimatorStableV1\src\main\deploy\UIv1"
+cd "C:\Users\Ricardo\Documents\GitHub\TouchScreenInterface\Example\src\main\deploy\UIv1"
 start http://localhost:8888
 pynetworktables2js --robot roborio-7563-frc.local
 
